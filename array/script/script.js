25afe5e5d4b59b178f5c1e76caa322ad459452d6
Jing Wang <ocpwj@yahoo.com>
2018-02-13 12:45:47 +0800

    no message

---
+++ array/script/script.js
@@ -0,0 +1,16 @@
+var studentName=[];
+studentName[0]="wang";
+studentName[1]="jing";
+studentName[2]="shi";
+studentName[3]="hao";
+studentName[4]="ren";
+
+document.write("studentName[4]="+studentName[4]+" and the length of this array is:"+studentName.length);
+
+
+var teacherName=["Computer", 100, "Cost",true];
+
+
+alert(teacherName.toString());
+
+
